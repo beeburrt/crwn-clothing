ab5f945385e9f4c0970981be598e9b7047b23898
Brian Burt <beeburrt@outlook.com>
2023-01-02 19:29:41 -0800

    Add nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import { Routes, Route } from "react-router-dom";
-
+import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>I am the shop page</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 }
