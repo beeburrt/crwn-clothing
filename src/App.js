e4b3ea232782dad1ada3c611924d6897ac420c9b
Brian Burt <beeburrt@outlook.com>
2023-01-06 19:45:39 -0800

    Add signInWithEmailAndPassword

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
-import SignIn from "./routes/sign-in/sign-in.component";
+import Authentication from "./routes/authentication/authentication.component";
 
 const Shop = () => {
   return (
@@ -11,16 +11,16 @@ const Shop = () => {
   );
 };
 
-function App() {
+const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="/shop" element={<Shop />} />
-        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
-}
+};
 
 export default App;
