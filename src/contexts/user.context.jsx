da48edd7a3044d4f770b4b180c1c5fa9525c3829
Brian Burt <beeburrt@outlook.com>
2023-01-07 23:32:57 -0800

    Add user context

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,9 +1,13 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const UserContext = createContext({
-
+  currentUser: null,
+  setCurrentUser: () => null,
 });
 
-export const UserProvider = ({children}) => {
-  return <UserContext.Provider>{children}</UserContext.Provider>
-}
+export const UserProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const value = { currentUser, setCurrentUser };
+  
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
