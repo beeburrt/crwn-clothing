69df2e890830ff628b403957b17b8beb960ed2a5
Brian Burt <beeburrt@outlook.com>
2023-01-27 01:37:06 -0800

    Change more files over to styled components

--- src/components/cart-dropdown/cart-dropdown.styles.js
+++ src/components/cart-dropdown/cart-dropdown.styles.js
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
-import { BaseButton, GoogleSignInButton, InvertedButton } from "../button/button.styles";
+import {
+  BaseButton,
+  GoogleSignInButton,
+  InvertedButton,
+} from "../button/button.styles";
 
 export const CartDropdownContainer = styled.div`
   position: absolute;
@@ -15,7 +19,7 @@ export const CartDropdownContainer = styled.div`
   right: 40px;
   z-index: 5;
 
-  ${(BaseButton}, 
+  ${BaseButton},
   ${GoogleSignInButton}, 
   ${InvertedButton} {
     margin-top: auto;
