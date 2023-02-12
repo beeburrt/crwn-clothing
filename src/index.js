7060ad81cbfb5468bf1e89053154147597e4578e
Brian Burt <beeburrt@outlook.com>
2023-02-12 12:38:07 -0800

    Add currentUser selector

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import App from "./App";
-import { UserProvider } from "./contexts/user.context";
 import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
+import { store } from "./store/store";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("👑")).render(
-  <BrowserRouter>
-    <UserProvider>
+  <Provider store={store}>
+    <BrowserRouter>
       <CategoriesProvider>
         <CartProvider>
           <App />
         </CartProvider>
       </CategoriesProvider>
-    </UserProvider>
-  </BrowserRouter>
+    </BrowserRouter>
+  </Provider>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -4,3 +4,11 @@ import {
   applyMiddleware,
 } from "redux";
 import logger from "redux-logger";
+
+import { rootReducer } from "./root-reducer";
+
+const middleWares = [logger];
+
+const composedEnhancers = compose(applyMiddleware(...middleWares));
+
+export const store = createStore(rootReducer, undefined, composedEnhancers);
